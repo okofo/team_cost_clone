bee3251b1471ece6647423b9b1c6f6f38e27cfc7
okofo <aidward32@gmail.com>
2021-12-20 21:41:54 +0000

    font change

--- lib/main.dart
+++ lib/main.dart
@@ -32,6 +32,7 @@ class MyApp extends StatelessWidget {
           debugShowCheckedModeBanner: false,
           theme: ThemeData(
             primarySwatch: Colors.blue,
+            fontFamily: 'Metropolis',
           ),
           initialRoute: '/',
           routes: {
